b78ad4408d4c05f665d5f3a8ed42a1e89e0334e1
Sourabh Bhavsar <sourabhbhavsar19@gmail.com>
2021-05-19 20:39:25 -0600

    Create Solution.java

---
+++ BinaryToHex/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        
+        StringBuilder sb = new StringBuilder("");
+        
+        char[] map = {'0','1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b' ,'c', 'd', 'e', 'f'};
+        
+        while (num != 0) {
+            int m = (num & 15);
+            sb.append(map[m]);
+            num = num >>> 4;
+        }
+        
+        return sb.reverse().toString();
+    }
+}
