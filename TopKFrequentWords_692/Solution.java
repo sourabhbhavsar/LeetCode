6eccaef7f48b9d88196ba4c308f10618ad8858c4
Sourabh Bhavsar <sourabhbhavsar19@gmail.com>
2022-01-01 00:35:25 -0700

    https://leetcode.com/problems/top-k-frequent-words/

---
+++ TopKFrequentWords_692/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
+        
+        PriorityQueue<Map.Entry<String, Integer>> pq = 
+            new PriorityQueue<>((a, b) -> 
+                                (a.getValue() == b.getValue() ? 
+                                 b.getKey().compareTo(a.getKey()) : a.getValue() - b.getValue()) );
+        
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            pq.offer(entry);
+            
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        
+        List<String> ans = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            ans.add(0, pq.poll().getKey());
+        }
+        
+        return ans;
+    }
+}
