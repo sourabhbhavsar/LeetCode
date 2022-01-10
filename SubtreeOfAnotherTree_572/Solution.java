bf36f44537587721aae37b7dc6425ddd02fb0779
Sourabh Bhavsar <sourabhbhavsar19@gmail.com>
2022-01-09 23:37:12 -0700

    https://leetcode.com/problems/subtree-of-another-tree/

---
+++ SubtreeOfAnotherTree_572/Solution.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        
+        if (areTreeCongruent(root, subRoot)) {
+            return true;
+        }
+         
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+    
+    
+    public boolean areTreeCongruent(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) {
+            return true;
+        }
+        
+        if (root1 == null || root2 == null) {
+            return false;
+        }
+        
+        if (root1.val != root2.val) {
+           return false;
+        }
+        
+        return areTreeCongruent(root1.left, root2.left) 
+            && areTreeCongruent(root1.right, root2.right) ;
+    }
+}
