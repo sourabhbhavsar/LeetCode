d516ecd2af08e9c803f322643afc7f3e07c34728
Sourabh Bhavsar <sourabhbhavsar19@gmail.com>
2022-01-16 14:25:13 -0700

    https://leetcode.com/problems/sum-of-unique-elements/

---
+++ SumOfUniqueElements_1748/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int[] freq = new int[101];
+        for (int n : nums) {
+            freq[n]++;
+        }
+        
+        int sum = 0;
+        for (int i = 0; i < freq.length; i++) {
+            if (freq[i] == 1) {
+                sum = sum + i;
+            }
+        }
+        
+        return sum;
+    }
+}
