1e2b26a9bec92fdb233ca404e597859194d32fb7
Sourabh Bhavsar <sourabhbhavsar19@gmail.com>
2022-01-30 11:24:46 -0700

    https://leetcode.com/problems/validate-stack-sequences/

--- ValidateStackSequences_946/Solution.java
+++ ValidateStackSequences_946/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean validateStackSequences(int[] pushed, int[] popped) {
+    public boolean validateStackSequences1(int[] pushed, int[] popped) {
         ArrayDeque<Integer> stack = new ArrayDeque<>();
         int index = 0;
         for (int i = 0; i < pushed.length; i++) {
@@ -14,4 +14,20 @@ class Solution {
         
         return stack.isEmpty();
     }
+    
+     public boolean validateStackSequences(int[] pushed, int[] popped) {
+        int i = 0;
+        int j = 0;
+        for (int k = 0; k < pushed.length; k++) {
+            pushed[i] = pushed[k];
+            i++;
+            while (i > 0 && pushed[i - 1] == popped[j]) {
+                i--;
+                j++;
+            }
+        }
+        
+        return i == 0;
+    }
+    
 }
